5637950e3f2e383a7a3ae93ac3a3bb062f08c1de
daniel daum <maliciouspickle@proton.me>
2024-11-10 19:55:07 -0800

    refactor: hello(), handle empty string

--- hello.go
+++ hello.go
@@ -3,6 +3,11 @@ package main
 import "fmt"
 
 func Hello(name string) string {
+
+	if name == "" {
+		return "Hello, world!"
+	}
+
 	return "Hello, " + name
 }
 
--- hello_test.go
+++ hello_test.go
@@ -2,11 +2,24 @@ package main
 
 import "testing"
 
-func TestHell0(t *testing.T) {
-	got := Hello("Chris")
-	want := "Hello, Chris"
+func TestHello(t *testing.T) {
 
-	if got != want {
-		t.Errorf("got %q does not equal want %q", got, want)
-	}
+	t.Run("Say hello to a person", func(t *testing.T) {
+		got := Hello("Chris")
+		want := "Hello, Chris"
+
+		if got != want {
+			t.Errorf("Got %q does not equal want %q", got, want)
+		}
+	})
+
+	t.Run("Say 'Hello, world!' when the string is empty", func(t *testing.T) {
+		got := Hello("")
+		want := "Hello, world!"
+
+		if got != want {
+			t.Errorf("Got %q does not equal want %q", got, want)
+		}
+	})
+
 }
