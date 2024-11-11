08bda4eeebd835f26864e067b505fab1e0fa076d
daniel daum <maliciouspickle@proton.me>
2024-11-10 19:45:30 -0800

    refactor Hello, accept a name

--- hello_test.go
+++ hello_test.go
@@ -3,8 +3,8 @@ package main
 import "testing"
 
 func TestHell0(t *testing.T) {
-	got := Hello()
-	want := "Hello, world!"
+	got := Hello("Chris")
+	want := "Hello, Chris"
 
 	if got != want {
 		t.Errorf("got %q does not equal want %q", got, want)
