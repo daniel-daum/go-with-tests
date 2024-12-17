1c6681f03caab292fb8c2d68b002fb35235d8f5c
Daniel Daum <maliciouspickle@proton.me>
2024-12-17 13:18:10 -0800

    update readme, completed errors/pointers

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 # go-with-tests
+
+Learning go via "Learn go with tests": https://quii.gitbook.io/learn-go-with-tests
---
+++ pointers_errors/wallet_test.go
@@ -0,0 +1,49 @@
+package pointerserrors
+
+import (
+	"testing"
+)
+
+func TestWallet(t *testing.T) {
+
+	assertBalance := func(t testing.TB, wallet Wallet, want Bitcoin) {
+		t.Helper()
+		got := wallet.Balance()
+
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	assertError := func(t testing.TB, got, want error) {
+		t.Helper()
+		if got == nil {
+			t.Fatal("didn't get an error but wanted one")
+		}
+		if got != want {
+			t.Errorf("got %s, want %s", got, want)
+		}
+	}
+
+	t.Run("deposit", func(t *testing.T) {
+		wallet := Wallet{}
+		wallet.Deposit(Bitcoin(10))
+		assertBalance(t, wallet, Bitcoin(10))
+	})
+
+	t.Run("withdraw", func(t *testing.T) {
+
+		wallet := Wallet{balance: Bitcoin(20)}
+		wallet.Withdraw(Bitcoin(15))
+		assertBalance(t, wallet, Bitcoin(5))
+	})
+
+	t.Run("withdraw insufficent funds", func(t *testing.T) {
+		startingBalance := Bitcoin(20)
+		wallet := Wallet{startingBalance}
+		err := wallet.Withdraw(Bitcoin(100))
+
+		assertBalance(t, wallet, startingBalance)
+		assertError(t, err, ErrInsufficientFunds)
+	})
+}
